a01efe2ca7c79c8943f156478584edda986011aa
marcell <ricardo.marcelino@ti.ukdw.ac.id>
2023-07-27 12:51:26 +0700

    Update UG2.py

--- UG2.py
+++ UG2.py
@@ -26,19 +26,19 @@ def main():
         diameter = float(input("Masukkan diameter (cm) : "))
         tinggi = float(input("Masukkan tinggi (cm) : "))
         volume = hitung_volume_tabung(diameter, tinggi)
-        print(f"Volume tabung adalah {volume:.2f} cm³")
+        print(f"Volume tabung adalah {volume:.2f} cm")
     elif pilihan == 2:
         sisi = float(input("Masukkan panjang sisi (cm) : "))
         volume = hitung_volume_kubus(sisi)
-        print(f"Volume kubus adalah {volume:.2f} cm³")
+        print(f"Volume kubus adalah {volume:.2f} cm")
     elif pilihan == 3:
         panjang = float(input("Masukkan panjang (cm) : "))
         lebar = float(input("Masukkan lebar (cm) : "))
         tinggi = float(input("Masukkan tinggi (cm) : "))
         volume = hitung_volume_balok(panjang, lebar, tinggi)
-        print(f"Volume balok adalah {volume:.2f} cm³")
+        print(f"Volume balok adalah {volume:.2f} cm")
     else:
-        print("Pilihan tidak valid. Silakan pilih 1, 2, atau 3.")
+        print("Inputan yang anda masukkan salah !!")
 
 if __name__ == "__main__":
     main()
